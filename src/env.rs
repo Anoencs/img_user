e2916d99051e49d33c5f48aeab5ede9396d38dee
Anoencs <thaian122001@gmail.com>
2024-03-04 10:52:31 +0700

    fix cors

--- src/env.rs
+++ src/env.rs
@@ -101,7 +101,6 @@ fn check_aws_region(aws_region: &str) -> Result<(), ValidationError> {
 
 pub fn setup_cors() -> CorsLayer {
     CorsLayer::new()
-        .allow_credentials(true)
         .allow_origin(Any)
         .allow_headers([
             ORIGIN,
